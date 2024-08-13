008a2cbbfcf608115b239f1f9c4b495a0e06b520
Gui <guicolleell@gmail.com>
2024-08-13 14:18:18 -0300

    Eita

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/')
+def styless():
+    return render_template("styless.css")
+
 if __name__ == '__main__':
     app.run(debug=True)
